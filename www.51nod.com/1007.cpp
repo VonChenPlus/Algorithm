9d86bd8712021e12fce588516666d3d2ce077165
VonChenPlus <VonChenPlus@gmail.com>
2015-06-13 20:26:05 +0800

    add some accept code

--- www.51nod.com/1007.cpp
+++ www.51nod.com/1007.cpp
@@ -1,24 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#define len 64*400
+#include<stdio.h>
+
+//void ex_gcd(long long a,long long b,long long &x,long long &y)
+//{ 
+//    if(b==0) 
+//    { 
+//        x=1,y=0; return; 
+//    }
+//
+//    ex_gcd(b,a%b,x,y); 
+//    
+//    long long t=x; x=y,y=t-a/b*y;
+//}
+
+long long exp_mod(long long a, long long b, long long m)//ab mod m
+{
+    long long d = 1, t = a;
+    while (b>0)
+    {
+        if (b & 0x1)d = (d*t) % m;
+        b = b >> 1;
+        t = (t*t) % m;
+    }
+    return d;
+}
 
 int main()
 {
-    int a[len], d[len], i, j, n, sum;
-    while (scanf("%d", &n) == 1)
+    long n;
+    const long long a = 3;
+    const long long b = 1000000007;
+    long long result;
+    while (scanf("%ld", &n) == 1)
     {
-        memset(d, 0, sizeof(d));
-        for (i = 0, sum = 0; i<n; i++)
-        {
-            scanf("%d", &a[i]);
-            sum += a[i];
-        }
-        for (i = 0; i<n; i++)
-        {
-            for (j = sum / 2; j >= a[i]; j--)
-                if (d[j - a[i]] + a[i]>d[j]) d[j] = d[j - a[i]] + a[i];
-        }
-        printf("%d\n", sum - d[sum / 2] * 2);
+        //long long x, y;
+        //ex_gcd(2, b, x, y);
+        //x = (x + b) % b;
+        long x = 500000004;
+        result = (exp_mod(a, n + 1, b) - 1) % b;
+        result *= x;
+        result %= b;
+        printf("%ld\n", result);
     }
+
     return 0;
 }
